db09750c894b897248d2ab2915000622098b1e3e
Kenji Yamada <clouddev822@gmail.com>
2021-02-07 05:21:02 -0500

    modify index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Drawer } from './Drawer'
-export { default as Loader } from './Loader'
-export { default as RoleTag } from './RoleTag'
-export * from './Forms'
+// export { default as Drawer } from "./Drawer";
+// export { default as Loader } from "./Loader";
+// export { default as RoleTag } from "./RoleTag";
+// export * from "./Forms";
